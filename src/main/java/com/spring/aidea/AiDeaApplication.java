9cee87932f6285114b51ae024ac5541270dcab16
songkey06 <mjsong0524@gmail.com>
2025-08-07 15:45:14 +0900

    초기설정과 .gitignore파일 설정

---
+++ src/main/java/com/spring/aidea/AiDeaApplication.java
@@ -0,0 +1,13 @@
+package com.spring.aidea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiDeaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiDeaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/aidea/AiDeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.aidea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiDeaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
