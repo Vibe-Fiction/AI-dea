2123eaf5137f4abede32202dda4f0c43310d29a2
rhehdgus8831 <rhehdgus8831@naver.com>
2025-08-09 15:20:14 +0900

    feat:  JWT 설정 파일 읽기 클래스 생성 #1

---
+++ src/main/java/com/spring/aidea/vibefiction/global/jwt/JwtProperties.java
@@ -0,0 +1,20 @@
+package com.spring.aidea.vibefiction.global.jwt;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * JWT 설정 파일 읽기 - application.yml 의 jwt.xx 값을 읽어오는 클래스입니다.
+ * @author 고동현
+ */
+@Setter @Getter
+@Configuration
+@ConfigurationProperties(prefix = "jwt")
+public class JwtProperties {
+
+    private String secret;
+    private String expiration;
+}
