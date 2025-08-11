c9696a824fba898f5c4c72f1535d64ff97bfcae0
Sirosho <pack8447@gmail.com>
2025-08-11 18:53:46 +0900

    쿼리매서드 최적화 시작

--- src/main/java/com/spring/aidea/vibefiction/dto/response/NovelsResponseDtoSH.java
+++ src/main/java/com/spring/aidea/vibefiction/dto/response/NovelsResponseDtoSH.java
@@ -27,6 +27,29 @@ public class NovelsResponseDtoSH {
     LocalDateTime laseUpdatedAt; // 최종 수정일시
     // 좋아요 추가해야함
 
+
+
+//    SELECT
+//    N.NOVEL_ID,
+//    N.TITLE,
+//    N.SYNOPSIS,
+//    N.COVER_IMAGE_URL,
+//    N.VISIBILITY,
+//    N.STATUS,
+//    N.VIEW_COUNT,
+//    N.CREATED_AT,
+//    N.LAST_UPDATED_AT,
+//    N.AUTHOR_ID,
+//    COUNT(CASE WHEN F.CATEGORY = 'LIKE' THEN 1 END) AS like_count,
+//    GROUP_CONCAT(DISTINCT G.NAME) AS GENRE_NAMES,
+//    GROUP_CONCAT(DISTINCT C.USER_ID) AS COLLABORATOR_USER_IDS
+//    FROM novels N
+//    LEFT JOIN novel_genres NG ON N.NOVEL_ID = NG.NOVEL_ID
+//    LEFT JOIN GENRES G ON NG.GENRE_ID = G.GENRE_ID
+//    LEFT JOIN FAVORITES F ON N.NOVEL_ID = F.NOVEL_ID
+//    LEFT JOIN COLLABORATORS C ON N.NOVEL_ID = C.NOVEL_ID
+//    GROUP BY N.NOVEL_ID
+
     public static NovelsResponseDtoSH from(Novels novels) {
 
         return NovelsResponseDtoSH.builder()
--- src/main/java/com/spring/aidea/vibefiction/repository/NovelsRepository.java
+++ src/main/java/com/spring/aidea/vibefiction/repository/NovelsRepository.java
@@ -19,8 +19,28 @@ public interface NovelsRepository extends JpaRepository<Novels, Long> {
      */
     // 소설 전체조회
     // @EntityGraph는 적어둔 필드에 필요한 테이블을 자동으로 Fetch Join을 해줌
-    @EntityGraph(attributePaths = {"author", "novelGenres.genre"})
-    @Query("select distinct n from Novels n") // 카티샨 곱 문제 해결을 위한 distinct 적용
+    @Query(value = """
+SELECT
+    N.NOVEL_ID,
+    N.TITLE,
+    N.SYNOPSIS,
+    N.COVER_IMAGE_URL,
+    N.VISIBILITY,
+    N.STATUS,
+    N.VIEW_COUNT,
+    N.CREATED_AT,
+    N.LAST_UPDATED_AT,
+    N.AUTHOR_ID,
+    COUNT(CASE WHEN F.CATEGORY = 'LIKE' THEN 1 END) AS like_count,
+    GROUP_CONCAT(DISTINCT G.NAME) AS GENRE_NAMES,
+    GROUP_CONCAT(DISTINCT C.USER_ID) AS COLLABORATOR_USER_IDS
+FROM novels N
+         LEFT JOIN novel_genres NG ON N.NOVEL_ID = NG.NOVEL_ID
+         LEFT JOIN GENRES G ON NG.GENRE_ID = G.GENRE_ID
+         LEFT JOIN FAVORITES F ON N.NOVEL_ID = F.NOVEL_ID
+         LEFT JOIN COLLABORATORS C ON N.NOVEL_ID = C.NOVEL_ID
+GROUP BY N.NOVEL_ID
+""", nativeQuery=true) // 카티샨 곱 문제 해결을 위한 distinct 적용
     List<Novels> findAllWithGenresAndAuthor();
 
     // 소설 단건 디테일 조회
