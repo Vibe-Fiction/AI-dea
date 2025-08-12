a90d9cdb67e9ec7f74aeac6fd388ecd53713826d
TJK98 <wtj1998@naver.com>
2025-08-12 19:58:34 +0900

    feat: Proposal 및 Chapter 조회를 위한 쿼리 메소드 추가 (#18)

--- src/main/java/com/spring/aidea/vibefiction/repository/ChaptersRepository.java
+++ src/main/java/com/spring/aidea/vibefiction/repository/ChaptersRepository.java
@@ -3,5 +3,9 @@ package com.spring.aidea.vibefiction.repository;
 import com.spring.aidea.vibefiction.entity.Chapters;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ChaptersRepository extends JpaRepository<Chapters, Long> {
+    Optional<Chapters>
+    findTop1ByNovelIdOrderByChapterNumberDesc(Long novelId);
 }
--- src/main/java/com/spring/aidea/vibefiction/repository/ProposalsRepository.java
+++ src/main/java/com/spring/aidea/vibefiction/repository/ProposalsRepository.java
@@ -3,6 +3,9 @@ package com.spring.aidea.vibefiction.repository;
 import com.spring.aidea.vibefiction.entity.Proposals;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProposalsRepository extends JpaRepository<Proposals, Long> {
+import java.util.List;
 
+public interface ProposalsRepository extends JpaRepository<Proposals, Long> {
+    List<Proposals>
+    findByChapterIdOrderByCreatedAtAsc(Long chapterId);
 }
