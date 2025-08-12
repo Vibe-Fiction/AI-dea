202f1d68a7fd041c6e650939a00b53622783a297
Sirosho <pack8447@gmail.com>
2025-08-12 15:53:52 +0900

    메인페이지, 챕터페이지, 투표페이지 html, css 추가 및 페이지 컨트롤러 반영

--- src/main/java/com/spring/aidea/vibefiction/global/config/SecurityConfig.java
+++ src/main/java/com/spring/aidea/vibefiction/global/config/SecurityConfig.java
@@ -27,6 +27,11 @@ public class SecurityConfig {
     // 여기에 각자 URL들 추가하면 자동적용
     String[] permitAllURLs = {
             "/api/novels/**",
+            "/",
+            "/css/**",
+            "/js/**",
+            "/api/chapters/**",
+            "/api/vote",
 
     };
 
---
+++ src/main/java/com/spring/aidea/vibefiction/routes/PageController.java
@@ -0,0 +1,27 @@
+package com.spring.aidea.vibefiction.routes;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j
+public class PageController {
+
+    // 홈으로 이동
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+    // 챕터페이지로 이동
+    @GetMapping("/api/chapters")
+    public String chapters() {
+        return "chapters-page";
+    }
+    // 투표페이지로 이동
+    @GetMapping("/api/vote")
+    public String vote() {
+        return "vote-page";
+    }
+
+}
